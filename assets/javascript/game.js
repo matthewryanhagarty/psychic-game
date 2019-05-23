4c9112d704a8ec65f64d3babcfdb70e7486cada4
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-05-22 21:50:46 -0700

    wrote main if/else statement for psychic-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,25 @@ var guessesLeft = [9];
 var guessedLetters = [];
 
 
-var random = letterOptions[Math.floor(Math.random() * letterOptions.length)];
+var computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];
 
-console.log(random)
+document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+    var computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];
+
+        if ( userGuess = computerGuess) {
+            wins++;
+        }
+            else {
+                guessesLeft--;
+                guessedLetters.push(userGuess);
+    }
+}
+
+document.onkeyup()
+
+var winsText = document.getElementById("winsText");
+var losesText = document.getElementById("losesText");
+var livesLeftText = document.getElementById("livesLeftText");
+var guessSoFarText = document.getElementById("guessesSoFarText");
