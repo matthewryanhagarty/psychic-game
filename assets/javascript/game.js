b0e85070308870fd4231f4ae266ac22c93fa462c
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-05-22 21:13:59 -0700

    create computer guess as math.random.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,78 +1,12 @@
-// <!-- make a variable letterOption with a value of array of letter options <A-Z>
+var letterOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// why? this is going to help us generate a random 
-
-// variable wins initially set to 0
-
-// variable loses intiation set to 0
-
-// variable guessesLeft intitally set to 9.
-
-// variable guessedLetters which is initially set to an empty array
-
-// ________________________
-
-// actions/ functions
-
-// adding letters that to guessedLetters array (adding arguement of a letter)
-
-//     - takes letters guessed and pushes into guessedLetters
-
-// decrement guess count
-
-//     - checks to see if guesses remaining is 0
-//     - if not, decrement down by one guesses guessesLeft
-//     - 0, increase loses by 1
-//     - lose game/ restart
-
-// increment wins
-//     - if the correct letter is selected, then wins goes up by 1 (reset) - must define reset in a function
-
-// increment losses
-//     - if the guess count goes = 0, then increment loss count by 1 (reset)
-//     - a loss is defined as guesscount = 0
-
-// guesses so far 
-//     -when the user guesses a letter, it needs to be pushed to the guessed letters Array
-//     -this needs to be displayed on the screen --- 
-    
-//     point.innerHTML | var "name" = document.getElementByID("ID NAME")
-
-// if
-
-// else
-
-//     if 
-
-.innerHTML
-//     event.onkeyup 
-
-//         - check to see if key pressed is equal to computer guess
-//         - if not, add it to lettersGuessed
-//         -check to see if guessCount
-//             -if is, increment loses, reset game
-//                     - set guesses back to 9
-//                     - empty the guess array -->
-
-Computer picks a random letter
-    the random letter is going to come from the alaphabet Array
-
-
-
-
-
-
-
-
-
-var letterOptions = ["a", "b", "c"];
 
 var wins = [];
-
 var loses = [];
-
 var guessesLeft = [9];
-
 var guessedLetters = [];
 
 
+var random = letterOptions[Math.floor(Math.random() * letterOptions.length)];
+
+console.log(random)
