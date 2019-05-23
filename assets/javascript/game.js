c4276b262de94e40daa97254637fc3ce7ec681f7
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-05-22 22:48:21 -0700

    created new function for game reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,40 @@
 var letterOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
-var wins = [];
-var loses = [];
-var guessesLeft = [9];
+var wins = 0;
+var loses = 0;
+var guessesLeft = 9;
 var guessedLetters = [];
 
 
-var computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];
+   // Create variables that hold references to the places in the HTML where we want to display things.
+var winsText = document.getElementById("wins-text");
+var loses = document.getElementById("lloses-text");
+var livesLeft = document.getElementById("lives-left-text");
+var guessSoFar = document.getElementById("guesses-so-far-text");
+
 
 document.onkeyup = function(event) {
 
     var userGuess = event.key;
+
     var computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];
 
-        if ( userGuess = computerGuess) {
+        if (userGuess = computerGuess) {
             wins++;
         }
             else {
                 guessesLeft--;
                 guessedLetters.push(userGuess);
+            }
     }
-}
+        if (guessesLeft === 0) {
+                function reset(){
+                    guessesLeft = 9;
+                    guessedLetters = [];
+                }
+        }
 
-document.onkeyup()
+// winsText.textContent = wins;
 
-var winsText = document.getElementById("winsText");
-var losesText = document.getElementById("losesText");
-var livesLeftText = document.getElementById("livesLeftText");
-var guessSoFarText = document.getElementById("guessesSoFarText");
+console.log(computerGuess)
